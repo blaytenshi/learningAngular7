acaf89028d39d3a01d8fbc303f742ed9a11fbb94
Jimmy Tang <blay.tenshi@gmail.com>
2019-04-28 20:07:49 +0800

    S2L19 Working with Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // either templateUrl or template property must be present
   templateUrl: './servers.component.html',
+  // Can either use template property to specify inline template
+  // code. Here, commented out is some inline code to render the
+  // app-server (not app-servers) component.
+  // You can even wrap the inline code with backticks if you
+  // need to multiline the code.
+  // template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
