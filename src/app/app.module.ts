b6775f1074a56e2882ece54e0b09edfea79b8408
Jimmy Tang <blay.tenshi@gmail.com>
2019-04-28 11:03:18 +0800

    S2E17 First custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// old @angular/http module has been deprecated
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
+  // Registers specific modules for Angular to scan and be
+  // 'aware' of to use in the application. Here we register
+  // the new ServerComponent that we just made
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   // This tells Angular what Component should self-start
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // Name of the selector should be a unique string.
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+
+})
+export class ServerComponent {
+
+}
